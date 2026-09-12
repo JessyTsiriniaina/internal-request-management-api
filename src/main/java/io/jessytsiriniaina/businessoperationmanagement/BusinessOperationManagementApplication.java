eb43449da7d17083661890892c10485cc13e28cf
Tsiriniaina Jessy <tsiriniaina.jessy@gmail.com>
2026-09-12 10:11:25 +0300

    first commit

---
+++ src/main/java/io/jessytsiriniaina/businessoperationmanagement/BusinessOperationManagementApplication.java
@@ -0,0 +1,13 @@
+package io.jessytsiriniaina.businessoperationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessOperationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessOperationManagementApplication.class, args);
+    }
+
+}
